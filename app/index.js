04e900aa7ded9aa5615b14fb4e115af5aebf90c3
alisha.dixon <alisha.dixon15@ed.cna.nl.ca>
2025-06-13 12:26:11 -0230

    done

--- app/index.js
+++ app/index.js
@@ -5,17 +5,17 @@ import SwitchButton from './components/SwitchButton';
 
 const data = [
   {
-    title: '#1 Call of Duty: Black Ops III',
-    description: 'Call of Duty: Black Ops III is a 2015 first-person shooter game developed by Treyarch and published by Activision. It is the twelfth entry in the Call of Duty series ',
+    title: '#1 - Call of Duty: Black Ops III',
+    description: 'Call of Duty: Black Ops III is a 2015 first-person shooter game developed by Treyarch and published by Activision. It is the twelfth entry in the Call of Duty series. ',
     image: require('./images/Black_Ops_3.jpg')
   },
   {
-    title: '#2 Rainbow 6 Siege',
+    title: '#2 - Rainbow Six Siege',
     description: 'Tom Clancy\'s Rainbow Six Siege is a 2015 tactical shooter game developed by Ubisoft Montreal and published by Ubisoft. The game puts heavy emphasis on environmental destruction and cooperation between players.',
     image: require('./images/images.jpg')
   },
   {
-    title: '#3 Minecraft',
+    title: '#3 - Minecraft',
     description: 'Minecraft is a game made up of blocks, creatures, and community. You can survive the night or build a work of art – the choice is all yours.',
     image: require('./images/minecraft.jpg')
   }
